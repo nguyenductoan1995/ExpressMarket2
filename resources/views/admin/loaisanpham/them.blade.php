1ada3a05e25e70967782835705c4509373d1fc44
hieudendi <30580912+hieudendi@users.noreply.github.com>
2018-04-04 23:25:45 +0700

    fix trang thêm loại sản phẩm

--- resources/views/admin/loaisanpham/them.blade.php
+++ resources/views/admin/loaisanpham/them.blade.php
@@ -2,16 +2,7 @@
 @extends('admin.layout.index')
 @section('content')
 			
-					<!-- PAGE CONTENT BEGINS -->
-	@if(count($errors) > 0 )
-		<div class="alert alert-danger">
-			@foreach($errors-> all() as err)
-				{{$err}}</br>
-
-			@endforeach
-		</div>
-	@endif
-
+	
 	@if(session('thongbao'))
 		<div class= "alert alert-success">
 			{{session('thongbao')}}
